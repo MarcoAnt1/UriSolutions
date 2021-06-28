235eb851092a592936291df716067f148477c324
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-06-28 12:27:32 -0300

    Solução e testes Uri1008.

---
+++ UriSolutions/Uri1008.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UriSolutions
+{
+    /// <summary>
+    /// Salário.
+    /// </summary>
+    public class Uri1008
+    {
+        private readonly int pesoA = 2;
+        private readonly int pesoB = 3;
+        private readonly int pesoC = 5;
+
+        public void Solution()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int hourTrabalhada = int.Parse(Console.ReadLine());
+            double valorRecebido = Convert.ToDouble(Console.ReadLine());
+
+            double salary = hourTrabalhada * valorRecebido;
+
+            Console.WriteLine($"NUMBER = {number}");
+            Console.WriteLine($"SALARY = U$ {salary:0.00}");
+            Console.ReadLine();
+        }
+
+        public Dictionary<int, string> SolutionForTests(int number, int hourTrabalhada, double valorRecebido)
+        {
+            double salary = hourTrabalhada * valorRecebido;
+
+            var retorno = new Dictionary<int, string>
+            {
+                { number, $"{salary:0.00}" }
+            };
+
+            return retorno;
+        }
+    }
+}
---
+++ UriSolutionsTests/Uri1008Tests.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using UriSolutions;
+
+namespace UriSolutionsTests
+{
+    [TestClass]
+    public class Uri1008Tests
+    {
+        private Uri1008 uri1008;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            uri1008 = new Uri1008();
+        }
+
+        [TestMethod]
+        public void Uri1003_Example_01()
+        {
+            Dictionary<int, string> retorno = uri1008.SolutionForTests(25, 100, 5.50);
+
+            Assert.IsTrue(retorno.ContainsKey(25));
+            Assert.IsTrue(retorno.ContainsValue("550.00"));
+        }
+
+        [TestMethod]
+        public void Uri1003_Example_02()
+        {
+            Dictionary<int, string> retorno = uri1008.SolutionForTests(1, 200, 20.50);
+
+            Assert.IsTrue(retorno.ContainsKey(1));
+            Assert.IsTrue(retorno.ContainsValue("4100.00"));
+        }
+
+        [TestMethod]
+        public void Uri1003_Example_03()
+        {
+            Dictionary<int, string> retorno = uri1008.SolutionForTests(6, 145, 15.55);
+
+            Assert.IsTrue(retorno.ContainsKey(6));
+            Assert.IsTrue(retorno.ContainsValue("2254.75"));
+        }
+    }
+}
