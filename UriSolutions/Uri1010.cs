601689c0185af907f6307fa83faaa1f29ece0207
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-06-28 15:40:52 -0300

    Solução e testes Uri1010.

---
+++ UriSolutions/Uri1010.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UriSolutions
+{
+    /// <summary>
+    /// Salário com Bônus.
+    /// </summary>
+    public class Uri1010
+    {
+        public void Solution()
+        {
+            string compra1 = Console.ReadLine();
+            string compra2 = Console.ReadLine();
+
+            string[] values1 = compra1.Split(' ');
+            int codPeca1 = int.Parse(values1[0]);
+            int numPeca1 = int.Parse(values1[1]);
+            double valorPeca1 = Convert.ToDouble(values1[2]);
+
+            string[] values2 = compra2.Split(' ');
+            int codPeca2 = int.Parse(values2[0]);
+            int numPeca2 = int.Parse(values2[1]);
+            double valorPeca2 = Convert.ToDouble(values2[2]);
+
+            double valorTotal = (numPeca1 * valorPeca1) + (numPeca2 * valorPeca2);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {valorTotal:0.00}");
+            Console.ReadLine();
+        }
+
+        public string SolutionForTests(string compra1, string compra2)
+        {
+            string[] values1 = compra1.Split(' ');
+            int numPeca1 = int.Parse(values1[1]);
+            double valorPeca1 = Convert.ToDouble(values1[2]);
+
+            string[] values2 = compra2.Split(' ');
+            int numPeca2 = int.Parse(values2[1]);
+            double valorPeca2 = Convert.ToDouble(values2[2]);
+
+            double valorTotal = (numPeca1 * valorPeca1) + (numPeca2 * valorPeca2);
+
+            return $"{valorTotal:0.00}";
+        }
+    }
+}
---
+++ UriSolutionsTests/Uri1010Tests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UriSolutions;
+
+namespace UriSolutionsTests
+{
+    [TestClass]
+    public class Uri1010Tests
+    {
+        private Uri1010 uri1010;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            uri1010 = new Uri1010();
+        }
+
+        [TestMethod]
+        public void Uri1010_Example_01()
+        {
+            string retorno = uri1010.SolutionForTests("12 1 5.30", "16 2 5.10");
+
+            Assert.AreEqual("15.50", retorno);
+        }
+
+        [TestMethod]
+        public void Uri1010_Example_02()
+        {
+            string retorno = uri1010.SolutionForTests("13 2 15.30", "161 4 5.20");
+
+            Assert.AreEqual("51.40", retorno);
+        }
+
+        [TestMethod]
+        public void Uri1010_Example_03()
+        {
+            string retorno = uri1010.SolutionForTests("1 1 15.10", "2 1 15.10");
+
+            Assert.AreEqual("30.20", retorno);
+        }
+    }
+}
