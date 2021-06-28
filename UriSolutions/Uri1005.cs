6ee9fa6e9ca2871a0041520b5309eed3ca0b5e08
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-06-28 10:32:08 -0300

    Solução Uri1005.

---
+++ UriSolutions/Uri1005.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UriSolutions
+{
+    /// <summary>
+    /// Média 1.
+    /// </summary>
+    public static class Uri1005
+    {
+        public static void Solution()
+        {
+            double pesoA = 3.5;
+            double pesoB = 7.5;
+
+            var a = Convert.ToDouble(Console.ReadLine());
+            var b = Convert.ToDouble(Console.ReadLine());
+
+            double media = (a * pesoA) + (b * pesoB) / (pesoA + pesoB);
+
+            Console.WriteLine($"MEDIA = {media:0.0000}");
+
+        }
+    }
+}
