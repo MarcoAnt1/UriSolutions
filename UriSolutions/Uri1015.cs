729e37ea203706a727c43437f21102f2d8215aba
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-06-29 12:28:35 -0300

    Solução e Testes Uri1015.

---
+++ UriSolutions/Uri1015.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UriSolutions
+{
+    /// <summary>
+    /// Dinstância Entre Dois Pontos.
+    /// </summary>
+    public class Uri1015
+    {
+        public void Solution()
+        {
+            string texto1 = Console.ReadLine();
+            string texto2 = Console.ReadLine();
+
+            string[] value1 = texto1.Split(' ');
+            double x1 = Convert.ToDouble(value1[0]);
+            double y1 = Convert.ToDouble(value1[1]);
+
+            string[] value2 = texto1.Split(' ');
+            double x2 = Convert.ToDouble(value2[0]);
+            double y2 = Convert.ToDouble(value2[1]);
+
+            double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine($"{distance:0.0000}");
+            Console.ReadLine();
+        }
+
+        public string SolutionForTests(string texto1, string texto2)
+        {
+            string[] value1 = texto1.Split(' ');
+            double x1 = Convert.ToDouble(value1[0]);
+            double y1 = Convert.ToDouble(value1[1]);
+
+            string[] value2 = texto2.Split(' ');
+            double x2 = Convert.ToDouble(value2[0]);
+            double y2 = Convert.ToDouble(value2[1]);
+
+            double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            return $"{distance:0.0000}";
+        }
+    }
+}
