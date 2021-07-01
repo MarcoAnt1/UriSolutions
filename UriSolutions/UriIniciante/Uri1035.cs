c3a868061bd19f94a324966cdf0e77b9e898ae0b
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-07-01 17:23:34 -0300

    Solução e Testes Uri1035.

---
+++ UriSolutions/UriIniciante/Uri1035.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace UriSolutions
+{
+    /// <summary>
+    /// Notas e moedas.
+    /// </summary>
+    public class Uri1035
+    {
+        public void Solution()
+        {
+            string texto = Console.ReadLine();
+
+            string[] value = texto.Split(' ');
+            int a = int.Parse(value[0]);
+            int b = int.Parse(value[1]);
+            int c = int.Parse(value[2]);
+            int d = int.Parse(value[3]);
+
+            string retorno;
+            if (b > c && d > a && (c + d) > (a + b) && (c > 0) && (d > 0) && (a % 2) == 0)
+            {
+                retorno = "Valores aceitors";
+            }
+            else
+            {
+                retorno = "Valores nao aceitos";
+            }
+
+            Console.WriteLine(retorno);
+            Console.ReadLine();
+        }
+
+        public string SolutionForTests(string texto)
+        {
+            string[] value = texto.Split(' ');
+            int a = int.Parse(value[0]);
+            int b = int.Parse(value[1]);
+            int c = int.Parse(value[2]);
+            int d = int.Parse(value[3]);
+
+            if (b > c && d > a && (c + d) > (a + b) && (c > 0) && (d > 0) && (a % 2) == 0)
+                return "Valores aceitos";
+            else
+                return "Valores nao aceitos";
+        }
+    }
+}
---
+++ UriSolutionsTests/UriIniciantesTests/Uri1035Tests.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UriSolutions;
+
+namespace UriSolutionsTests
+{
+    [TestClass]
+    public class Uri1035Tests
+    {
+        private Uri1035 uri1035;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            uri1035 = new Uri1035();
+        }
+
+        [TestMethod]
+        public void Uri1021_Example_01()
+        {
+            string retorno = uri1035.SolutionForTests("5 6 7 8");
+
+            Assert.AreEqual("Valores nao aceitos", retorno);
+        }
+
+        [TestMethod]
+        public void Uri1021_Example_02()
+        {
+            string retorno = uri1035.SolutionForTests("2 3 2 6");
+
+            Assert.AreEqual("Valores aceitos", retorno);
+        }
+    }
+}
